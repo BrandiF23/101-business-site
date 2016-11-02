82760d8705a4be22294476c4152269a0533259cd
Brandi Flink <brandi_flink@yahoo.com>
2016-11-02 14:25:42 -0500

    adding first code and style changes

---
+++ services.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+
+	<div class="header">
+		<img src="Images/logo.png" class="header__logo">
+			<ul class="navigation">
+				<li>Home</li>
+				<li>Pricing</li>
+				<li>Masseurs</li>
+				<li>Services</li>
+				<li>Contacts</li>
+				<li>Book an appointment</li>
+			</ul>
+	</div>
+
+	<div class="services-block">
+		<div class="services-block__content">
+			<img src="Images/services-01.jpg" class="image">
+			<div class="title">
+				Swedish Massage
+			</div>
+			<img src="Images/index-04.png" class="decrotive__img">
+			<div class="description">
+				Swedish massage is basically a framework for all other types of Western massage practices and techniques. This includes sports massage, deep tissue massage, and aromatherapy massage.
+			</div>
+		</div>
+
+		<div class="services-block__content">
+			<img src="Images/services-02.jpg" class="image">
+			<div class="title">
+				Deep Tissue Massage
+			</div>
+			<img src="Images/index-04.png" class="decrotive__img">
+			<div class="description">
+				Deep tissue massage is a therapeutical massage technique which focuses on realigning deeper layers of muscles in the human boday. Revitalized energy and improved mood are generally the end effect of this type of body massage.
+			</div>
+		</div>
+
+		<div class="services-block__content">
+			<img src="Images/services-01.jpg" class="image">
+			<div class="title">
+				Hot Stone Therapy
+			</div>
+			<img src="Images/index-04.png" class="decrotive__img">
+			<div class="description">
+				Hot stone massage was designed as a special kind of revitalizing technique. This type of massage sees the therapist using slick and pre-heated stones as an extension of their own hands.
+			</div>
+		</div>
+	</div>
+
+		<div class="services-block">
+		<div class="services-block__content">
+			<img src="Images/services-01.jpg" class="image">
+			<div class="title">
+				Aromatherapy Massage
+			</div>
+			<img src="Images/index-04.png" class="decrotive__img">
+			<div class="description">
+				Aromatherapy massage is a specifically developed massage therapeutical practice, that employs highly concentrated plant oils and miscellaneous scents, called essential oils.
+			</div>
+		</div>
+
+		<div class="services-block__content">
+			<img src="Images/services-01.jpg" class="image">
+			<div class="title">
+				Revitalizing Massage
+			</div>
+			<img src="Images/index-04.png" class="decrotive__img">
+			<div class="description">
+				While the massage is itself an absolutely delightful and relaxing procedure, some of them don't have to be all smooth and slow.
+			</div>
+		</div>
+
+		<div class="services-block__content">
+			<img src="Images/services-01.jpg" class="image">
+			<div class="title">
+				Sports Massage
+			</div>
+			<img src="Images/index-04.png" class="decrotive__img">
+			<div class="description">
+				SPorts massage is one of the most popular forms of treatment for soft tissure injuries and for speefing the post-recovery up! Getting at least a weekly sports massage has additional advantages.
+			</div>
+		</div>
+	</div>
+
+	<div class="footer">
+		<img src="??" class="footer-logo">
+		<div class="contact-phone">
+			<img src="??" class="phone-img">
+			<div class="contact-title">
+				Call Today:
+			</div>
+			<div class="contact-detail">
+				555-123-5353<br>
+				555-123-2323
+			</div>
+		</div>
+		<div class="contact-email">
+			<img src="??" class="email-img">
+			<div class="contact-title">
+				E-mail:
+			</div>
+			<div class="contact-detail">
+				<a href="info@demolink.org"></a>
+			</div>
+		</div>
+		<div class="contact-address">
+			<img src="??" class="address-img">
+			<div class="contact-title">
+				Address:
+			</div>
+			<div class="contact-detail">
+				1163 N State St.<br>
+				Chicago, IL 69610
+			</div>
+		</div>
+		<div class="contact-hours">
+			<img src="??" class="hours-img">
+			<div class="contact-title">
+				Opening Hours:
+			</div>
+			<div class="contact-detail">
+				Mon-Fri: 9am-6pm;<br>
+				Sun: 10am-1pm
+			</div>
+		</div>
+
+		<div class="privacy-policy">
+			2016 Fiona. Privacy Policy
+		</div>
+		
+	</div>
+
+
+</body>
+</html>
