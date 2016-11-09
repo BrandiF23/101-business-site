15e819c08d3e47fbb3c2b331739aca95fe73ee5d
Brandi Flink <brandi_flink@yahoo.com>
2016-11-09 17:47:07 -0600

    adding code to homepage

---
+++ homepage.php
@@ -0,0 +1,92 @@
+<?php include ('header.php'); ?>
+
+<div class="homepage-topImage-wrapper">
+	
+	<div class="homepage-topImage">
+		<div class="homepage-topImage__title">
+			Highly customized upscale massage for your pleasure
+		</div>
+		<div class="homepage-topImage__title-sub">
+			Call us for help and set yourself up with a massage session that will be relaxing for both your body and your spirit!
+		</div>
+	</div>
+	
+</div>
+
+<div class="homepage-content">
+
+	<div class="homepage-content__block">
+		<div class="homepage-content__block-title">
+			Affordability
+		</div>
+		<img src="Images/index-04.png" class="homepage-content__block-img">
+		<div class="homepage-content__block-description">
+		Something good should not always be unattainably priced. We aim to bring the balance to your quality/cost ratio, making it affordable for all citizens of Chicago!
+		</div>
+	</div>
+
+	<div class="homepage-content__block">
+		<div class="homepage-content__block-title">
+			Variety
+		</div>
+		<img src="Images/index-04.png" class="homepage-content__block-img">
+		<div class="homepage-content__block-description">
+			Our Chicago salon features the most diverse and universally list of massage types. From intensive sports massage all the way to pre and post natal, or full body massage.
+		</div>
+	</div>
+
+	<div class="homepage-content__block--special">
+		<div class="homepage-content__block-title--special">
+			Flexibility
+		</div>
+		<img src="Images/index-04.png" class="homepage-content__block-img--special">
+		<div class="homepage-content__block-description--special">
+			We undetsand that people rarely have time to go to the massage salon during the working day or week. So we work 24/7, on-site, and on the weekends as well!
+		</div>
+	</div>
+</div>
+
+<!-- <div class="homepageImg-cropper"> -->
+	<img src="Images/index-05.jpg" class="homepage-middleImg">
+<!-- </div> -->
+
+
+<div class="services-price-block">
+	<div class="services-price-block__title">
+			See a full list of our massage services here:
+	</div>
+	
+	<div class="service-price-block__content">
+		<div class="service-price-block__content-title">
+			<div class="img-cropper">
+				<img class="service-price-block__content-img" src="Images/index-13.jpg">
+			</div>
+				Swedish Massage
+		</div>
+
+		<div class="service-price-block__content-title">
+			<div class="img-cropper">
+				<img class="service-price-block__content-img" src="Images/index-13.jpg">
+			</div>
+				Deep Tissue Massage
+		</div>
+
+		<div class="service-price-block__content-title">
+			<div class="img-cropper">
+				<img class="service-price-block__content-img" src="Images/index-12.jpg">
+			</div>
+				Hot Stone Massage
+		</div>
+
+	</div>
+
+		<div class="homepage-services-button">
+			<a href="services.php" class="homepage-services-button__link">See all Services</a>
+		</div>
+	</div>
+</div>
+
+
+
+
+<?php include ('footer.php'); ?>
