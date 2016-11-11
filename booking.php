8ee39f32cdf51fe763c02d82cfddcf6cd987f1d3
Brandi Flink <brandi_flink@yahoo.com>
2016-11-10 18:52:21 -0600

    adding booking page

---
+++ booking.php
@@ -0,0 +1,86 @@
+<?php include ('header.php'); ?>
+
+<div class="calendar-wrapper">
+
+	<div class="calendar-block">
+		<div class="arrow-left"><</div>
+		<div class="month">October 2016</div>
+		<div class="arrow-right">></div>
+	</div>
+
+	<table class="calendar-block__month">
+		<tr class="weekdays">
+			<td>Sun</td>
+			<td>Mon</td>
+			<td>Tue</td>
+			<td>Wed</td>
+			<td>Thu</td>
+			<td>Fri</td>
+			<td>Sat</td>
+		</tr>
+
+		<tr class="date">
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td>1</td>
+		</tr>
+
+		<tr>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+			<td>6</td>
+			<td>7</td>
+			<td>8</td>
+		</tr>
+
+		<tr>
+			<td>9</td>
+			<td>10</td>
+			<td>11</td>
+			<td>12</td>
+			<td>13</td>
+			<td>14</td>
+			<td>15</td>
+		</tr>
+
+		<tr>
+			<td>16</td>
+			<td>17</td>
+			<td>18</td>
+			<td>19</td>
+			<td>20</td>
+			<td>21</td>
+			<td>22</td>
+		</tr>
+
+		<tr>
+			<td>23</td>
+			<td>24</td>
+			<td>25</td>
+			<td>26</td>
+			<td>27</td>
+			<td>28</td>
+			<td>29</td>
+		</tr>
+
+		<tr>
+			<td>30</td>
+			<td>31</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	</table>
+
+
+</div>
+
+<?php include ('footer.php'); ?>
