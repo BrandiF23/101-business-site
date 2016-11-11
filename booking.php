72c16b9e1119062b1f89c8bc11797576125ef59c
Brandi Flink <brandi_flink@yahoo.com>
2016-11-10 20:52:55 -0600

    finished booking page

--- booking.php
+++ booking.php
@@ -1,86 +1,81 @@
 <?php include ('header.php'); ?>
 
-<div class="calendar-wrapper">
-
-	<div class="calendar-block">
-		<div class="arrow-left"><</div>
-		<div class="month">October 2016</div>
-		<div class="arrow-right">></div>
-	</div>
-
 	<table class="calendar-block__month">
+		<tr class="month">
+			<td colspan="1" class="arrow">&lt;</td>
+			<td colspan="6" class="month__current">October 2016</td>
+			<td colspan="1" class="arrow">&gt;</td>
+		</tr>
 		<tr class="weekdays">
-			<td>Sun</td>
-			<td>Mon</td>
-			<td>Tue</td>
-			<td>Wed</td>
-			<td>Thu</td>
-			<td>Fri</td>
-			<td>Sat</td>
+			<td colspan="1" class="weekdays__day">Sun</td>
+			<td colspan="1" class="weekdays__day">Mon</td>
+			<td colspan="1" class="weekdays__day">Tue</td>
+			<td colspan="1" class="weekdays__day">Wed</td>
+			<td colspan="1" class="weekdays__day">Thu</td>
+			<td colspan="1" class="weekdays__day">Fri</td>
+			<td colspan="1" class="weekdays__day">Sat</td>
+		</tr>
+
+		<tr class="date-top">
+			<td class="date-top__day"></td>
+			<td class="date-top__day"></td>
+			<td class="date-top__day"></td>
+			<td class="date-top__day"></td>
+			<td class="date-top__day"></td>
+			<td class="date-top__day"></td>
+			<td class="date-top__day">1</td>
 		</tr>
 
 		<tr class="date">
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td>1</td>
+			<td class="date__day">2</td>
+			<td class="date__day">3</td>
+			<td class="date__day">4</td>
+			<td class="date__day">5</td>
+			<td class="date__day">6</td>
+			<td class="date__day">7</td>
+			<td class="date__day">8</td>
 		</tr>
 
-		<tr>
-			<td>2</td>
-			<td>3</td>
-			<td>4</td>
-			<td>5</td>
-			<td>6</td>
-			<td>7</td>
-			<td>8</td>
+		<tr class="date">
+			<td class="date__day-closed">9</td>
+			<td class="date__day-closed">10</td>
+			<td class="date__day">11</td>
+			<td class="date__day">12</td>
+			<td class="date__day">13</td>
+			<td class="date__day">14</td>
+			<td class="date__day">15</td>
 		</tr>
 
-		<tr>
-			<td>9</td>
-			<td>10</td>
-			<td>11</td>
-			<td>12</td>
-			<td>13</td>
-			<td>14</td>
-			<td>15</td>
+		<tr class="date">
+			<td class="date__day">16</td>
+			<td class="date__day">17</td>
+			<td class="date__day">18</td>
+			<td class="date__day">19</td>
+			<td class="date__day">20</td>
+			<td class="date__day">21</td>
+			<td class="date__day">22</td>
 		</tr>
 
-		<tr>
-			<td>16</td>
-			<td>17</td>
-			<td>18</td>
-			<td>19</td>
-			<td>20</td>
-			<td>21</td>
-			<td>22</td>
+		<tr class="date">
+			<td class="date__day">23</td>
+			<td class="date__day">24</td>
+			<td class="date__day">25</td>
+			<td class="date__day">26</td>
+			<td class="date__day">27</td>
+			<td class="date__day">28</td>
+			<td class="date__day">29</td>
 		</tr>
 
-		<tr>
-			<td>23</td>
-			<td>24</td>
-			<td>25</td>
-			<td>26</td>
-			<td>27</td>
-			<td>28</td>
-			<td>29</td>
-		</tr>
-
-		<tr>
-			<td>30</td>
-			<td>31</td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
+		<tr class="date">
+			<td class="date__day">30</td>
+			<td class="date__day-31">31</td>
+			<td class="date__day"></td>
+			<td class="date__day"></td>
+			<td class="date__day"></td>
+			<td class="date__day"></td>
+			<td class="date__day"></td>
 		</tr>
 	</table>
 
-
-</div>
 
 <?php include ('footer.php'); ?>
